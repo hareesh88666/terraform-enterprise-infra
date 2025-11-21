376fc4b5d7c4f05a49c506d54b70156a282ecf8e
Cloud User <ec2-user@ip-172-31-27-129.eu-north-1.compute.internal>
2025-11-21 16:01:22 +0000

    updated modules

--- environments/stg/main.tf
+++ environments/stg/main.tf
@@ -45,3 +45,28 @@ module "bucket" {
   environment  = var.environment
   name_suffix  = "appdata"
 }
+
+
+module "multi_vm" {
+  source = "../../modules/gcp-multi-vm-module"
+
+  project_id           = var.project_id
+  subnetwork_self_link = module.subnets.subnetwork_self_links["private-1"]
+
+  vms = {
+    backend = {
+      vm_name          = "backend-rhel"
+      zone             = "asia-south1-a"
+      machine_type     = "e2-medium"
+      assign_public_ip = false
+    }
+
+    frontend = {
+      vm_name          = "frontend-rhel"
+      zone             = "asia-south1-a"
+      machine_type     = "e2-small"
+      assign_public_ip = true
+    }
+  }
+}
+
--- modules/gcp-subnets-module/outputs.tf
+++ modules/gcp-subnets-module/outputs.tf
@@ -12,3 +12,9 @@ output "subnet_names" {
   description = "List of subnet names created"
   value = keys(google_compute_subnetwork.this)
 }
+
+output "subnetwork_self_links" {
+  description = "Map of subnet name to self_link"
+  value = { for k, v in google_compute_subnetwork.this : k => v.self_link }
+}
+
